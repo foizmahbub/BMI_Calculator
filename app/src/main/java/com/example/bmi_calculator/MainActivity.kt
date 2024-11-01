bb44321e8b307506dea8fdd285f43dc22ef8a9fb
foizmahbub <foizmahbub01@gmail.com>
2024-11-01 19:34:14 +0600

    update_BG

--- app/src/main/java/com/example/bmi_calculator/MainActivity.kt
+++ app/src/main/java/com/example/bmi_calculator/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.bmi_calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.bmi_calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,13 +13,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.ShowBtn.setOnClickListener {
 
+
             val height = binding.Edt1.text.toString().toDouble()/100
             val weight =binding.Edt2.text.toString().toDouble()
             val bmi=weight/(height*height)
 
+
             binding.BMITv.text =String.format("BMI is : %2f",bmi)
             binding.BMItypeTv.text=when(bmi){
-                in 1.00..18.4->"Under_weihgt"
+                in 1.00..18.4->"Under_weight"
                 in 18.5..25.5->"Perfect"
                 in 25.6..30.50->"Over Weight"
 
